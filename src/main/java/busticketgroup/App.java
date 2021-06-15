4e422a3b394e9dd007081cc8cc6ebf3570f6051f
testops1 <84893243+testops1@users.noreply.github.com>
2021-06-15 16:14:25 +0530

    Add files via upload
    added src files

---
+++ src/main/java/busticketgroup/App.java
@@ -0,0 +1,13 @@
+package busticketgroup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
